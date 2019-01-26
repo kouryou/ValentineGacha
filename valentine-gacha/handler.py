87e1125110eefae6ded3a713bf2cb6154c532ca5
kouryou <23131663+kouryou@users.noreply.github.com>
2019-01-26 09:25:29 +0000

    [add] post selected user to slack

--- valentine-gacha/handler.py
+++ valentine-gacha/handler.py
@@ -7,20 +7,42 @@ import random
 
 def select_random_user(event, context):
 
+    # 環境変数読み込み
+    notice_channel = os.environ["SLACK_NOTICE_CHANNEL"]
     slack_token = os.environ["SLACK_API_TOKEN"]
+
+    # SlackClient作成
     sc = SlackClient(slack_token)
 
+    # ユーザー一覧取得
     users = sc.api_call(
         "users.list"
     )
+
+    # ランダムで一人選択
     members = users["members"]
-    random_user = random.choice(members)
+    selected_user = random.choice(members)
 
-    selected_user_name = random_user["profile"]["real_name"]
+    # メッセージに必要なユーザー情報格納
+    selected_user_name = selected_user["profile"]["real_name"]
+    selected_user_id = selected_user["id"]
 
+    # 選ばれたユーザー情報をログ出力
     logger = logging.getLogger()
     logger.warn(selected_user_name)
+    logger.warn(selected_user_id)
 
+    # 通知用メッセージ組み立て
+    send_text = "<@" + selected_user_id + ">\n" + "選ばれたのは、" + selected_user_name + "でした。 :tea:"
+
+    # slack通知
+    sc.api_call(
+        "chat.postMessage",
+        channel=notice_channel,
+        text=send_text
+    )
+
+    # レスポンス作成
     response = {
         "name": selected_user_name
     }
